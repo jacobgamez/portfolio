ca299aca1cd8ec37d4247145838ec438a2e54855
Jacob Gamez <jacob.a.gamez@gmail.com>
2015-02-05 11:35:44 -0800

    Added first Java file

---
+++ Java/LinkedList/basicLinkedList.java
@@ -0,0 +1,31 @@
+//An example of a basic linked list in Java
+
+class LL
+{
+    private Node head;
+    private int count;
+
+    //Linked List Constructor
+    public LL() 
+    {
+	head = new Node(null);
+	listCount = 0;
+    }
+
+    //Appends element to end of list
+    public void addEnd(obj data)//arbitrary terms
+    {
+	Node temp = new Node(data);
+	Node curr = head;
+
+	while (curr.getNext() != null)
+	    {
+		curr = curr.getNext();
+	    }
+
+	curr.setNext(temp);
+	count++;
+    }
+}
+
+//etc.
